a99974c0149743e3e25ec1bfd188ed9620c5f9ef
Destin Lee <dleeinc@gmail.com>
2024-12-01 22:29:01 +0000

    cleaning

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,4 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
+
 export default function Search() {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ['pokemons'],
+    staleTime: 1000 * 60 * 60 * 24,
+    queryFn: async () => {
+      const response = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?limit=1050',
+      )
+      const { results } = await response.json()
+
+      return results
+    },
+  })
+
+  console.log(isLoading, isFetching, data)
+
+  if (isLoading || isFetching) {
+    return <div>Loading...</div>
+  }
+
+  if (!data) {
+    return <div>No Data!</div>
+  }
+
   return (
     <div className='max-w-lg m-auto p-4'>
       <form action=''>
