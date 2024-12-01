c0f1ae95619d27f7745290b933b1f23e52a416fb
Destin Lee <destin.lee@a2zsync.com>
2024-12-01 17:15:38 -0600

    basic search

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,38 +1,95 @@
 import { useQuery } from '@tanstack/react-query'
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router'
+
+interface PokemonSimp {
+  name: string
+  url: string
+}
 
 export default function Search() {
+  const nav = useNavigate()
+
   const { data, isLoading, isFetching } = useQuery({
     queryKey: ['pokemons'],
     staleTime: 1000 * 60 * 60 * 24,
     queryFn: async () => {
       const response = await fetch(
-        'https://pokeapi.co/api/v2/pokemon?limit=1050',
+        'https://pokeapi.co/api/v2/pokemon?limit=1040',
       )
-      const { results } = await response.json()
+      const { results } = (await response.json()) as { results: PokemonSimp[] }
 
-      return results
+      return results.map((p, i) => {
+        const id = i + 1
+
+        return {
+          ...p,
+          id,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+        }
+      })
     },
   })
 
-  console.log(isLoading, isFetching, data)
+  const searchRef = useRef<HTMLInputElement>(null!)
+  const [filtered, setFiltered] = useState<PokemonSimp[]>(null!)
 
   if (isLoading || isFetching) {
-    return <div>Loading...</div>
+    return <div className='max-w-lg m-auto p-4'>Loading...</div>
   }
 
   if (!data) {
-    return <div>No Data!</div>
+    return <div className='max-w-lg m-auto p-4'>No Data!</div>
+  }
+
+  const handleSearchChange = () => {
+    const search = searchRef.current.value.toLowerCase()
+
+    if (!search || search.length < 2) {
+      setFiltered(null!)
+      return
+    }
+
+    const filtered = data.filter(p => p.name.includes(search))
+
+    setFiltered(filtered)
+  }
+
+  const handlePokemonClick = (id: number) => {
+    nav(`/pokemon/${id}`)
   }
 
   return (
     <div className='max-w-lg m-auto p-4'>
-      <form action=''>
+      <div>
         <input
           type='text'
-          className='w-full p-2 border border-gray-300 rounded'
-          placeholder='Search for a Pokemon'
+          ref={searchRef}
+          onChange={handleSearchChange}
+          className='w-full p-2 border border-gray-300 rounded bg-transparent mb-8'
+          placeholder={data ? `Search pokemon...` : 'Loading...'}
+          disabled={!data}
         />
-      </form>
+        {filtered ? (
+          <div className='flex flex-col gap-2'>
+            {filtered.map(p => (
+              <div
+                key={p.id}
+                className='flex justify-between items-center cursor-pointer hover:border-orange-400 pr-4 border-2 rounded-sm'
+                onClick={() => handlePokemonClick(p.id)}
+              >
+                <div className='flex items-center '>
+                  <img src={p.image} alt={p.name} />
+                  <h3 className='text-3xl'>{p.name}</h3>
+                </div>
+                <div className='text-slate-400 font-extrabold text-3xl'>
+                  # {p.id}
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
