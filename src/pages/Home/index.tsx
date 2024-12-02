c2d8c8d2b09d06c2ec2f2402582b04d828ca0927
Destin Lee <destin.lee@a2zsync.com>
2024-12-02 13:56:28 -0600

    working stuff

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,52 @@
-import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { FaCodeCompare } from 'react-icons/fa6'
+import { Link } from 'react-router'
+
+const apps = [
+  {
+    title: 'Type Checker',
+    icon: <FaCodeCompare />,
+    description: 'Check the effectiveness of a type against another type',
+    route: '/typecheck',
+  },
+  {
+    title: 'Search',
+    icon: <FaSearch />,
+    description: 'Search for a Pokemon by name or ID',
+    route: '/search',
+  },
+]
 
 export default function Home() {
-  return <div className='max-w-lg m-auto p-4'>Home</div>
+  return (
+    <div className='max-w-lg m-auto px-4'>
+      <div className='m-auto px-8 pb-8 text-center flex flex-col items-center'>
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png'
+          alt=''
+          className='w-32 h-32 inline'
+        />
+        <h1 className='mb-4 text-lg font-bold'>Welcome to Pokemon Utils!</h1>
+        <p>
+          This is a collection of tools to help you learn more about Pokemon and
+          help with the various games.
+        </p>
+      </div>
+      <div className='flex flex-col gap-4'>
+        {apps.map((app, i) => (
+          <Link
+            to={app.route}
+            key={i}
+            className='p-4 border-2 border-slate-700 rounded-md flex gap-4 items-center hover:border-orange-400 focus:border-orange-400'
+          >
+            {app.icon}
+            <div>
+              <h2 className='text-xl font-bold'>{app.title}</h2>
+              <p>{app.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
